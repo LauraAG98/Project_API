1f5bec6623cc6bd7734a9377b228df1765f6242d
LauraAG98 <lauraguirregarcia@outlook.com>
2026-07-06 14:09:37 -0500

    first commit

---
+++ src/app/services/character.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CharacterService {
+
+  //Se inyecta dependencia
+  constructor(private http: HttpClient) { }
+  
+  //Método para obtener paginación
+  getCharacters(page: number = 1) {
+    return this.http.get(`${environment.apiUrl}/character?page=${page}`); 
+  }
+
+  //Método para obtener personaje por id
+  getCharacterById(id: number) {
+    return this.http.get(`${environment.apiUrl}/character/${id}`);
+  }
+
+  //Método para obtener personaje por nombre
+  getCharacterByName(name: string) {
+    return this.http.get(`${environment.apiUrl}/character?name=${name}`)
+  }
+
+  //Método para obtener personaje por estado
+  getCharacterByStatus(status: string) {
+    return this.http.get(`${environment.apiUrl}/character?status=${status}`)
+  }
+
+  //Método para obtener por género
+  getCharacterByGender(gender: string) {
+    return this.http.get(`${environment.apiUrl}/character?gender=${gender}`) 
+  }
+}
